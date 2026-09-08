ca7a5672cdf9ee23303259fe9428fe26fa9d00ee
Lorena Gomez <lg@zazu.systems>
2026-09-08 10:27:29 +0200

    Added nextjs-dashboard code

---
+++ nextjs-dashboard/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/app/ui/login-form';
+
+export default function LoginPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-50 p-6">
+      <LoginForm />
+    </main>
+  );
+}
